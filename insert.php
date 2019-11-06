b796116b4a01c3996cd39c36209d425177d4f90d
namishkhanna <namishkhanna2351@gmail.com>
2019-11-06 21:38:40 +0530

    University Website

---
+++ insert.php
@@ -0,0 +1,72 @@
+<?php
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$inputAddress=$_POST['inputAddress'];
+$phone=$_POST['phone'];
+$pincode=$_POST['pincode'];
+$feedback=$_POST['feedback'];
+
+if (!empty($name) || !empty($email) || !empty($inputAddress) || !empty($phone) || !empty($pincode) || !empty($feedback))
+{
+
+$host="localhost";
+$dbUsername ="root";
+$dbPassword ="";
+$dbname="project";
+
+$conn = new mysqli($host , $dbUsername , $dbPassword , $dbname);
+
+if(mysqli_connect_error())
+{
+
+die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+
+}else
+{
+
+$SELECT ="SELECT email From register where email = ? Limit 1";
+$INSERT ="INSERT Into register(name,email,inputAddress,phone,pincode,feedback) values(?,?,?,?,?,?)";
+
+$stmt =$conn->prepare($SELECT);
+$stmt->bind_param("s",$email);
+$stmt->execute();
+$stmt->bind_result($email);
+$stmt->store_result();
+$rnum =$stmt->num_rows;
+
+if($rnum==0)
+{
+	$stmt->close();
+	$stmt=$conn->prepare($INSERT);
+	$stmt->bind_param("sssiis",$name,$email,$inputAddress,$phone,$pincode,$feedback);
+	$stmt->execute();
+include 'index.html';
+	echo'<script type="text/javascript">
+	window.onload=function(){alert("Thankyou We Will Contact You Shortly");}
+	</script>';
+
+	//echo "<font size='18' face='Arial' color='darkblue'><center><br><br><br><br>Thanks We Will Contact You Shortly</center>";
+}else
+{
+	include 'index.html';
+	echo'<script type="text/javascript">
+	window.onload=function(){alert("Email Already Registered");}
+	</script>';
+
+	//echo "<font size='18' face='Arial' color='red'><center><br><br><br><br>Email Already Registered</center>";
+}
+$stmt->close();
+$conn->close();
+
+}
+
+}else
+{
+
+echo "All Fields are Required";
+die();
+
+}
+
+?>
